e895c86be5104220ccf8d39b227f167fd71d8919
SurajKale01 <surajkale9140@gmail.com>
2025-07-26 01:32:35 +0530

    Refactor database connection logic into a separate connectDB module

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import cors from "cors";
 import authRoutes from "./routes/auth.routes";
 import contentRoutes from "./routes/content.routes";
 import shareLinkRoutes from "./routes/sharelink.routes";
+import connectDB from "./models/connectDB";
 
 const app = express();
 
@@ -17,22 +18,7 @@ app.use("/api/v1", authRoutes);
 app.use("/api/v1", contentRoutes);
 app.use("/api/v1", shareLinkRoutes);
 
-interface MainApp {
-  (app: express.Express): Promise<void>;
-}
-
-const main: MainApp = async (app) => {
-  try {
-    await mongoose.connect(process.env.MONGO_URL as string);
-
-    console.log("MongoDB connected!");
-
-    app.listen(3000, () => {
-      console.log(`Server is running on port 3000`);
-    });
-  } catch (error) {
-    console.error("Failed to connect: ", error);
-  }
-};
-
-main(app);
+app.listen(3000, () => {
+  connectDB();
+  console.log(`Server is running on port 3000`);
+});
