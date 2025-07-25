2e2bd81e25a3474569ce6a1547b25d13facb98d5
SurajKale01 <surajkale9140@gmail.com>
2025-07-25 23:50:32 +0530

    Add authentication middleware for token verification and user identification

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,30 @@
+import { Request, Response, NextFunction } from "express";
+import jwt, { JwtPayload } from "jsonwebtoken";
+
+export const authMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const authHeader = req.headers["authorization"];
+    if (!authHeader) {
+      return res.status(401).json({ message: "Authorization header missing" });
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    if (!token) {
+      return res.status(401).json({ message: "Token not found" });
+    }
+
+    const decoded = jwt.verify(
+      token,
+      process.env.JWT_USER_SECRET as string
+    ) as JwtPayload;
+    req.userId = decoded.userId;
+    next();
+  } catch (error) {
+    return res.status(403).json({ message: "Invalid or expired token" });
+  }
+};
