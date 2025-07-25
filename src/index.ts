7cf1b5832d1d9a743321ee063491d8d1a8d65d1f
SurajKale01 <surajkale9140@gmail.com>
2025-07-25 23:02:48 +0530

    Add initial project setup with dotenv configuration and dependencies

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+interface MainApp {
+  (app: express.Express): Promise<void>;
+}
+
+const main: MainApp = async (app) => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL as string);
+
+    console.log("MongoDB connected!");
+
+    app.listen(3000, () => {
+      console.log(`Server is running on port 3000`);
+    });
+  } catch (error) {
+    console.error("Failed to connect: ", error);
+  }
+};
+
+main(app);
