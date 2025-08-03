1f3aee36c241fb404ddbb9d5019f195e42a02788
SurajKale01 <surajkale9140@gmail.com>
2025-08-03 17:52:17 +0530

    added the cors origins

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,12 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://virtual-brain.vercel.app"],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
